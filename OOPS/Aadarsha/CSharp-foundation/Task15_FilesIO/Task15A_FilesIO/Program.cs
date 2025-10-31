90fbcc2696703bfa712415ca10e8942194390d5c
Aadarsha Khadka <aadar.kdk@gmail.com>
2025-10-31 13:52:50 +0545

    task: learn C# file handling basics

---
+++ OOPS/Aadarsha/CSharp-foundation/Task15_FilesIO/Task15A_FilesIO/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        string filePath = "example.txt";
+
+        // Write text to the file
+        File.WriteAllText(filePath, "Let's learn C# file handling!");
+
+        // read text from the file
+        string content = File.ReadAllText(filePath);
+        Console.WriteLine("File Content: " + content);
+
+        // Append text to the file
+        if (File.Exists(filePath))
+        {
+            File.AppendAllText(filePath, "\nAppending some more text at " + DateTime.Now);
+            Console.WriteLine("File updated successfully.");
+        }
+        else
+        {
+            Console.WriteLine("File not found!.");
+        }
+        string updated = File.ReadAllText(filePath);
+        Console.WriteLine("Updated file content: " + updated);  
+    }
+}
